382c0ca51c371479099867240f72179cb7ae34a2
Jhonny Costa <jhonnycosta@ibm.com>
2023-09-30 15:32:17 -0300

    add notificacoes

---
+++ src/app/notificacoes/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Layout from "@/components/Default/Layout";
+
+
+export default function Notificacoes() {
+  return (
+    <Layout title="Notificacoes" subtitle="Gerenciar Notificacoes">
+      <h3>Conteudo!!!</h3>
+    </Layout>
+  )
+}
